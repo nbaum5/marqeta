6a584b9ff9b794551f975e667d7e4860084e995e
Nicolas Baum <nbbaum@gmail.com>
2017-12-11 16:10:22 -0800

    more endpoints

--- lib/marqeta/api.rb
+++ lib/marqeta/api.rb
@@ -66,6 +66,17 @@ module Marqeta
     end
   end
 
+  class BulkCardOrder
+    def self.create(options)
+      path = "/bulkissuances"
+      Request.post(path, options)
+    end
+    def self.find(token)
+      path = "/bulkissuances/#{token}"
+      Request.get(path)
+    end
+  end
+
   class Order
     def self.create_gpa_order(options)
       path = "/gpaorders"
@@ -89,6 +100,21 @@ module Marqeta
     def self.list(options)
       path = "/transactions"
       Request.get(path, options)
+    end
+  end
+
+  class CardProduct
+    def self.create(options)
+      path = "/cardproducts"
+      Request.post(path, options)
+    end
+    def self.find(token)
+      path = "/cardproducts/#{token}"
+      Request.get(path)
+    end
+    def self.list
+      path = "/cardproducts"
+      Request.get(path)
     end
   end
 end
